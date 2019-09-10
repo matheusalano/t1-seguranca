625309027f1eaad766da1409734e59385eae0a1a
Matheus Alano <matheus_dias@terceiros.sicredi.com.br>
2019-09-09 21:23:03 -0300

    Problem with the keyword. It's generating `ahelino` instead of `avelino`. Why?

--- crypto_analysis.py
+++ crypto_analysis.py
@@ -62,67 +62,75 @@ def frequency_count(_dict_, letter):
     if _dict_.__contains__(letter):
         _dict_.update({letter: _dict_[letter]+1})
 
+def find_total_difference(list1, list2):
+    sum = 0
+    for index in range(len(list1)) : 
+      sum += abs(list1[index] - list2[index])
+    
+    return sum
 
-def vigenere_test(values):
+def rotate_list(old_list):  #Code provided
+     new_list = old_list[:]
+     new_list.append(old_list[0])
+     del new_list[0]
+     return new_list
+
+def key_generator(values, key_length):
     # TODO: criar dict dinamicamente pelo tamanho da chave
 
     base_dict = dict({
-        "a": 0,
-        "b": 0,
-        "c": 0,
-        "d": 0,
-        "e": 0,
-        "f": 0,
-        "g": 0,
-        "h": 0,
-        "i": 0,
-        "j": 0,
-        "k": 0,
-        "l": 0,
-        "m": 0,
-        "n": 0,
-        "o": 0,
-        "p": 0,
-        "q": 0,
-        "r": 0,
-        "s": 0,
-        "t": 0,
-        "u": 0,
-        "v": 0,
-        "w": 0,
-        "x": 0,
-        "y": 0,
-        "z": 0
+        "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0
     })
 
-    first_letter = base_dict.copy()
-    second_letter = base_dict.copy()
-    third_letter = base_dict.copy()
-    fourth_letter = base_dict.copy()
-    fifth_letter = base_dict.copy()
-    sixth_letter = base_dict.copy()
-    seventh_letter = base_dict.copy()
-    for words in values:
-        frequency_count(first_letter, words[0])
-        frequency_count(second_letter, words[1])
-        frequency_count(third_letter, words[2])
-        frequency_count(fourth_letter, words[3])
-        frequency_count(fifth_letter, words[4])
-        frequency_count(sixth_letter, words[5])
-        frequency_count(seventh_letter, words[6])
+    pt_freq = [.1463, .0104, .0388, .0499, .1257, .0102, .0130, .0128, .0618, .0040, .0002, .0278, .0474, .0505, .1073, .0252, .0120, .0653, .0781, .0434, .0463, .0167, .0001, .0021, .0001, .0047]
 
-    for (key, value) in sorted(first_letter.items()):
-        print(f"First Letter = {key} Count = {value}")
+    cosets = [None]*key_length
 
+    for i in range(key_length):
+        letter_feq = base_dict.copy()
+        for word in values:
+            frequency_count(letter_feq, word[i])
+        cosets[i] = letter_feq
+
+    key = ""
+    for coset in cosets:
+        cosetFreqList = list(coset.values())
+        alphaList = list(coset)
+    
+        differenceList = []
+        for index in range(len(alphaList)) :
+            differenceList.append(find_total_difference(cosetFreqList, pt_freq))
+            cosetFreqList = rotate_list(cosetFreqList)
+
+        letter_index = differenceList.index(min(differenceList))
+        letter = alphaList[letter_index]
+        key += letter
+
+    return key
+
+def vigenere_decode(ciphertext, key, alphabet):
+  ciphertext = ciphertext
+  cipherList = list(ciphertext)
+  keyList = list(key)
+  alphaList = list(alphabet)
+  decodeText = ""
+  
+  for index in range(len(ciphertext)) : 
+    cipherIndex = alphabet.index(cipherList[index])
+    keyIndex = alphabet.index(((keyList[index % len(key)])))
+    decodeTextIndex = (cipherIndex - keyIndex) % (26)
+    decodeText += alphabet[decodeTextIndex]
+    
+  return decodeText
 
 def main():
     filepath = sys.argv[1]
     f = open(filepath, "r")
     contents = f.read()
-    keyword_lenght = kasiski_test(contents)
-    values = prepare_data(keyword_lenght, contents)
-    vigenere_test(values)
-
+    keyword_length = kasiski_test(contents)
+    values = prepare_data(keyword_length, contents)
+    keyword = key_generator(values, keyword_length)
+    print(vigenere_decode(contents, keyword, 'abcdefghijklmnopqrstuvwxyz'))
 
 if __name__ == "__main__":
     main()
